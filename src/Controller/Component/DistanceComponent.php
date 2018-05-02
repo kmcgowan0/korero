a350c4e4fa4bb58792fd565bc13ca986e49f2217
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-05-02 17:16:33 +0100

    component to work out distance

--- src/Controller/Component/DistanceComponent.php
+++ src/Controller/Component/DistanceComponent.php
@@ -6,13 +6,24 @@ use Cake\Controller\Component;
 
 class DistanceComponent extends Component
 {
-    public function getLatLong()
+    public function getDistance($central_location, $location, $earthRadius = 3959)
     {
         //work out latitude and longitude based on user location in db
-    }
+        list($lat_from, $long_from) = explode(',', $central_location);
+        list($lat_to, $long_to) = explode(',', $location);
 
-    public function findDistance()
-    {
-        //work out distance between user location and related user location
+        $latFrom = deg2rad($lat_from);
+        $lonFrom = deg2rad($long_from);
+        $latTo = deg2rad($lat_to);
+        $lonTo = deg2rad($long_to);
+
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) +
+            pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+        $angle = atan2(sqrt($a), $b);
+        return $angle * $earthRadius;
+
     }
 }
