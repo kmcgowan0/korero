94eeba6f012c0f84c9a2993c5ef7cc30ece5e5d5
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-05-10 14:08:41 +0100

    getting user data

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -263,7 +263,8 @@ class UsersController extends AppController
     {
         $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
+            $user_data = $this->request->getData();
+            $user = $this->Users->patchEntity($user, $user_data);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
