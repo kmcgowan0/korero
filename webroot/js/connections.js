4d77d0a614fba7c2f61a71e0338a3b460c5e8a2a
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-05-12 19:12:49 +0100

    changing line positions and adding extra line if more than 4

--- webroot/js/connections.js
+++ webroot/js/connections.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var width = mainUser.width();
     var height = mainUser.height();
     var mainX = offset.left + width / 2;
-    var mainY = offset.top + height / 2 - 80;
+    var mainY = offset.top + height / 2 - 60;
 
     var relatedUsersCoords = [];
 
@@ -36,6 +36,7 @@ $(document).ready(function () {
 
         // add the (currently invisible) line to the page
 
+        console.log(y[2]);
         jQuery.each(y[2], function (i) {
             var id = 'line_' + new Date().getTime();
             var line = "<div id='" + id + "'class='line' title='" + this['name'] + "'>&nbsp;</div>";
@@ -52,8 +53,8 @@ $(document).ready(function () {
                 left = x1;
             }
             if (transform > 180) {
-                top = y1  + (i * 20);
-                left = x1  + (i * 10);
+                top = y1  + (i * 5);
+                left = x1  + (i * 1);
             }
             if (transform > 90) {
                 top = y1  + (i * 5);
@@ -64,12 +65,57 @@ $(document).ready(function () {
                 left = x1  + (i * 10);
             }
 
+            //rotate the line
+            $('#' + id).css({
+                'left': left - (i*5),
+                'top': top - (i*4),
+                'width': '0px',
+                'height': '4px',
+                'z-index': '10',
+                'background-color': '#' + color,
+                'position': 'absolute',
+                'transform': 'rotate(' + transform + 'deg)',
+                'transform-origin': '0px 0px',
+                '-ms-transform': 'rotate(' + transform + 'deg)',
+                '-ms-transform-origin': '0px 0px',
+                '-moz-transform': 'rotate(' + transform + 'deg)',
+                '-moz-transform-origin': '0px 0px',
+                '-webkit-transform': 'rotate(' + transform + 'deg)',
+                '-webkit-transform-origin': '0px 0px',
+                '-o-transform': 'rotate(' + transform + 'deg)',
+                '-o-transform-origin': '0px 0px'
+            });
 
-            console.log(x1 + ' left: ' + left + ' top: ' + top);
+            // 'draw' the line
+            $('#' + id).animate({
+                width: d
+            }, 600, "linear", function () {
+            });
+            console.log(i);
+            return i < 3;
+        });
+
+        //extra line
+        if (y[2].length > 3) {
+            var extras = y[2].length - (4);
+            var others;
+            if (extras == 1) {
+                others = extras + ' other';
+            } else {
+                others = extras + ' others';
+            }
+            var id = 'line_' + new Date().getTime();
+            var line = "<div id='" + id + "'class='line' title='" + others + "'>&nbsp;</div>";
+            // var color = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+            var color = '000';
+            $('#canvas').append(line);
+
+            var left = x1;
+            var top = y1;
 
             //rotate the line
             $('#' + id).css({
-                'left': left,
+                'left': left - (6),
                 'top': top,
                 'width': '0px',
                 'height': '4px',
@@ -93,8 +139,7 @@ $(document).ready(function () {
                 width: d
             }, 600, "linear", function () {
             });
-
-        });
+        }
 
     });
 
